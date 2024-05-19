187090bcd106ac22bb16f1b7b2a618791ddc2a56
Imtiaz Ali <imtiaz.ali@stud.uni-bamberg.de>
2024-05-19 19:57:56 +0200

    added user login

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,69 @@
+
+import { Button } from '@/components/ui/button';
+import { SignInButton, SignUpButton, SignedIn, SignedOut } from '@clerk/nextjs';
+import { ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
+import { Loader} from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link';
+
+const Page =()=>{  
+    return (
+        <div>
+            <div className="max-w-[988px] mx-auto flex-1 w-full flex-1 w-full
+            flex flex-col lg:flex-row items-center">
+                <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+                    <Image src="/hero.svg" fill alt="Hero" />
+                </div>
+            </div>
+            <div className='flex flex-col items-center gap-y-8'>
+                <h1 className='text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center'>
+                    Learn, practice, and master new languages with duo.
+                </h1>
+            </div>
+            <div>
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <SignedOut >
+                        <SignUpButton
+                            mode='modal'
+                            fallbackRedirectUrl="/learn"
+                            signInFallbackRedirectUrl="/learn"
+                        >
+                            <Button size={'lg'} 
+                                variant={"secondary"}
+                                className='w-full'
+                            >
+                                Get Started
+                            </Button>
+                        </SignUpButton>
+                        <SignInButton
+                            mode='modal'
+                            fallbackRedirectUrl="/learn"
+                            signUpFallbackRedirectUrl="/learn"
+                        >
+                            <Button size={'lg'}
+                                variant={"primaryOutline"}
+                                className='w-full'
+                            >
+                                I already have an account
+                            </Button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <Button size={"lg"} variant={"secondary"}
+                                className="w-full mt-5"
+                        >
+                            <Link href="/learn">
+                                Continue Learning
+                            </Link>
+
+                        </Button>
+                    </SignedIn>
+                </ClerkLoaded>
+            </div>
+        </div>
+    )
+}
+export default Page;
