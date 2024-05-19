bd5e3bdded70fc442cbda8bb1737e38f96e93118
Imtiaz Ali <imtiaz.ali@stud.uni-bamberg.de>
2024-05-19 22:58:04 +0200

    added flags in footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,9 +1,23 @@
-
+import { Button } from "@/components/ui/button"
+import Image from 'next/image'
 
 export const Footer = () =>{
     return (
         <footer className="hidden  lg:block h-20 w-full border-t-2  border-slate-200 p-2">
-            footer 
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="/flags/de.svg" alt="Russian" height={32} width={40} className="mr-4 rounded-md"/>
+                    Deutsch
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="/flags/ru.svg" alt="Russian" height={32} width={40} className="mr-4 rounded-md"/>
+                    Russian
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="/flags/jp.svg" alt="Russian" height={32} width={40} className="mr-4 rounded-md"/>
+                    Japenese
+                </Button>
+            </div> 
         </footer>
     )
 }
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,19 +8,17 @@ import Link from 'next/link';
 
 const Page =()=>{  
     return (
-        <div>
-            <div className="max-w-[988px] mx-auto flex-1 w-full flex-1 w-full
-            flex flex-col lg:flex-row items-center">
-                <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
-                    <Image src="/hero.svg" fill alt="Hero" />
+        <div className=' max-w-[988px] mx-auto flex-1 w-full flex it  flex-col lg:flex-row items-center'>
+            <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+                <Image src="/hero.svg" fill alt="Hero" />
+            </div>
+         
+            <div className=' flex-1 bg-white flex flex-col items-center gap-y-3  pl-4 w-full' > 
+                <div className='flex flex-col items-center gap-y-8'>
+                    <h1 className='text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center'>
+                        Learn, practice, and master new languages with duo.
+                    </h1>
                 </div>
-            </div>
-            <div className='flex flex-col items-center gap-y-8'>
-                <h1 className='text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center'>
-                    Learn, practice, and master new languages with duo.
-                </h1>
-            </div>
-            <div>
                 <ClerkLoading>
                     <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
                 </ClerkLoading>
@@ -53,7 +51,7 @@ const Page =()=>{
                     </SignedOut>
                     <SignedIn>
                         <Button size={"lg"} variant={"secondary"}
-                                className="w-full mt-5"
+                                className="w-[320px] mt-5"
                         >
                             <Link href="/learn">
                                 Continue Learning
